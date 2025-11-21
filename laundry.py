742e0c99cd579361c12f6f9b058e7998d90cb2c2
aryakimi <aryaluqhakim26@gmail.com>
2025-11-21 22:20:57 +0700

    hoho

--- laundry.py
+++ laundry.py
@@ -12,3 +12,10 @@ print("****************************************************")
 
 for i in range(1,21):
     print("ke-",i)
+    print("----------------------------------------------------")
+    print("   -Muhamad Adli Akbar                              ")
+    print("   -Novaka Rizky Heny Saputra                       ")
+    print("   -Restu Aji Prasetyo                              ")
+    print("   -Rifky Al Adli                                   ")
+    print("   -Arya Luqmannul Hakim                            ")
+    print("----------------------------------------------------")
