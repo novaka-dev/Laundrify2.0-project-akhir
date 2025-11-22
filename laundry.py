cc8d8490738af517d489464cd247de24a5ecf938
novaka-dev <vaka.coding@gmail.com>
2025-11-22 15:47:12 +0700

    vakadsdsd

--- laundry.py
+++ laundry.py
@@ -9,3 +9,26 @@ print("   -Restu Aji Prasetyo                              ")
 print("   -Rifky Al Adli                                   ")
 print("   -Arya Luqmannul Hakim                            ")
 print("****************************************************")
+
+for i in range(1,21):
+    print("ke-",i)
+    print("----------------------------------------------------")
+    print("   -Muhamad Adli Akbar                              ")
+    print("   -Novaka Rizky Heny Saputra                       ")
+    print("   -Restu Aji Prasetyo                              ")
+    print("   -Rifky Al Adli                                   ")
+    print("   -Arya Luqmannul Hakim                            ")
+    print("----------------------------------------------------")
+
+
+
+
+
+
+
+
+print("----------------------------------------------------")
+print("----------------------------------------------------")
+print("----------------------------------------------------")
+
+print("branch devvv")
