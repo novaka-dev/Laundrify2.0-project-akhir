57a36699ec13ff450ccafe45b65fe3c23bccfc11
muhamad adli <muhmdadli01@gmail.com>
2025-12-03 01:01:40 +0700

    update detail order

--- lihat_detail_order.py
+++ lihat_detail_order.py
@@ -6,20 +6,20 @@ import uuid
 def detai_order():
     orders = data_json(FILE_ORDER)
 
-    print(f"{"No":3} {'Id Pelanggan':15} {'Id Order':15} {'Nama Pelanggan':20} {'No Telp':15} {'Alamat':15} {'Tanggal Estimasi'}")
-    print("-" * 105)
+    print(f"{"No":4} {'Id Pelanggan':18} {'Id Order':18} {'Nama Pelanggan':18} {'No Telp':15} {'Alamat':50} {'Tanggal Estimasi'}")
+    print("-" * 150)
 
     # Loop tiap order
     for i, order in enumerate(orders , start=1):
         id_pelanggan = order["customer"].get("id", "")
-        id_order = order.get("order_id", "")
+        id_order = order.get("id", "")
         nama = order["customer"].get("name", "")
-        no_telp = order["customer"].get("no_telp", "")
-        alamat = order["customer"].get("alamat", "")
+        no_telp = order["customer"].get("phone", "")
+        alamat = order["customer"].get("address", "")
         tanggal_estimasi = order.get("tanggal_diterima", "")
 
-        print(f"{i:<3} {id_pelanggan:15} {id_order:15} {nama:20} {no_telp:15} {alamat:15} {tanggal_estimasi}")
+        print(f"{i:<4} {id_pelanggan:18} {id_order:18} {nama:18} {no_telp:15} {alamat:50} {tanggal_estimasi}")
 
-    print("-" * 105)
+    print("-" * 150)
 
 detai_order()
